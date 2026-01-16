3522f4723cb6a1e3c3ea2a0cb801d79bd0ee4d38
BookerGG <bookergg1@gmail.com>
2026-01-16 00:55:57 +0000

    Turn in commit

---
+++ REPORT.md
@@ -0,0 +1,6 @@
+### Part 1:
+
+What was already given was the first test case that tested the tick frequencies for 2hz. It tests if it is correctly functioning by checking whether there is a 50% load count by counting every time there is a tick. I added the test cases that tested the tick frequencies for 5hz and 50hz. They both do the same thing as the 2hz test case that was already there except there are now different frequencies. 
+
+### Part 2: 
+![alt text](GTKimage.png)
--- gen_tick.v
+++ gen_tick.v
@@ -1,11 +1,11 @@
 //=========================================================================
 // Name & Email must be EXACTLY as in Gradescope roster!
-// Name: 
-// Email: 
+// Name: Booker Gillies
+// Email: bgill021@ucr.edu
 // 
-// Assignment name: 
-// Lab section: 
-// TA: 
+// Assignment name: Clocks and Test Benches
+// Lab section: 1
+// TA: Allan Knight
 // 
 // I hereby certify that I have not received assistance on this assignment,
 // or used code, from ANY outside source other than the instruction team
@@ -20,12 +20,22 @@ module gen_tick # ( parameter SRC_FREQ = 5000, parameter TICK_FREQ = 1) (
 );
 
 // Declare registers and wires here
-
+integer limit = SRC_FREQ / TICK_FREQ / 2 - 1; 
+integer accumulator = 0;
+reg tick_out = 0;
 always @(posedge src_clk) begin
-    // put your code for the multiplier here
+    if (enable)  begin
+        accumulator <= accumulator + 1;
+        if (accumulator >= limit) begin
+            tick_out <= ~tick_out;
+            accumulator <= 0;
+        end
+    end else begin
+        tick_out <= 0;
+    end
 end
 
 // Change this assign statement to the actual tick value
-assign tick = src_clk;
+assign tick = tick_out;
 
 endmodule
